9fb389fadc872421483aa4f931a06220af98c2f8
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-08-04 18:33:23 +0530

    Added styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,27 @@
 //import AddIcon from '@mui/icons-material/Add';
 //import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import {useSelector} from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const handle = useSelector(state => state.user.handle)
   return (
-    <div className="w-full fixed top-0 h-16 border-b-2 border-b-gray-200 p-2 flex justify-between items-center bg-white">
-      <Link to="/" className="font-bold text-xl">ClubConnect IIITD</Link>
-      <div className="flex space-x-4">
-        <Link to="/clubs">Clubs</Link>
-        <Link to={`/users/${handle}`}>My Profile</Link>
+    <div className="bg-slate-800 w-full top-0 h-16 border-b-2 border-b-gray-200 p-5 flex justify-between items-center">
+      <Link to="/" className="text-slate-50 font-bold text-xl">ClubConnect IIITD</Link>
+      <div className="text-slate-50 p-4 flex space-x-10 border-solid">
+
+        <NavLink to="/" className={({ isActive }) => isActive ? "px-4 py-2 rounded-lg bg-slate-600" : "px-4 py-2"}>
+          Home
+        </NavLink>
+
+        <NavLink to="/clubs" className={({ isActive }) => isActive ? "px-4 py-2 rounded-lg bg-slate-600" : "px-4 py-2"}>
+          Clubs
+        </NavLink>
+
+        <NavLink to={`/users/${handle}`} className={({ isActive }) => isActive ? "px-4 py-2 rounded-lg bg-slate-600" : "px-4 py-2"}>
+          Profile
+        </NavLink>
+
       </div>
     </div>
   )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,10 +10,14 @@ const Login = () => {
   }
 
   return (
-    <div className="flex justify-center items-center w-full min-h-screen bg-slate-100">
-      <div className="flex flex-col items-center w-1/2 bg-white p-5 border-2 border-gray-200 space-y-4 shadow-lg">
-        <h1 className="font-semibold text-4xl underline">ClubConnect</h1>
-        <a href={`${apiBaseUrl}/auth/google`} className="border-2 p-2">Sign in with Google</a>
+    <div className="flex justify-center items-center w-full min-h-screen bg-slate-100 bg-gradient-to-r from-cyan-500 to-indigo-500">
+      <div className="flex flex-col items-center w-1/3 h-1/3 bg-white p-5 rounded-xl border-gray-200 shadow-lg">
+        <h1 className="font-semibold text-4xl mt-40 mb-10">ClubConnect IIIT-Delhi</h1>
+        <div className='border-2 rounded-lg p-3 flex mb-32'>
+          <img src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-icon-png-transparent-background-osteopathy-16.png" alt="google-login-logo" className='w-5 h-5 mr-3'/>
+          <a href={`${apiBaseUrl}/auth/google`}>Sign in with Google</a>
+        </div>
+        <p className='text-gray-500 italic'>A project by Indraprastha Institute of Information Technology, Delhi</p>
       </div>
     </div>
   )

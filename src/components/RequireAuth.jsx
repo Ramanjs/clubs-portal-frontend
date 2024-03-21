bd8eb1028ab829430ef63bb45879e6d50646f2f2
Aditya Girdhar <92713380+AdityaGirdhar@users.noreply.github.com>
2024-03-21 11:00:28 +0530

    Update RequireAuth.jsx - Added comments

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -2,12 +2,15 @@ import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const RequireAuth = (props) => {
+  // Access the loggedIn state from Redux store
   const loggedIn = useSelector(state => state.user.loggedIn);
 
+  // If user is not logged in, redirect to login page
   if (!loggedIn) {
     return <Navigate to="/login" replace />
   }
 
+  // If user is logged in, render the children components
   return props.children;
 }
 
